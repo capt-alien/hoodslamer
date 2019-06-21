047a0f917e771b520877853208081b61d583d7c0
capt-alien <eric.botcher@presidio.edu>
2019-06-20 18:43:03 -0700

    first kinda view

--- hoodslam/wrestlers/views.py
+++ hoodslam/wrestlers/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world, this is MOTHERFUCKING HOODSLAM!!")
+
+def wrestler(request, name):
+    return HttpResponse("This is the page for %s" % name)
+
+def match(request, match_id):
+    #pull fighters from the match and displays a page with the 
+    pass
