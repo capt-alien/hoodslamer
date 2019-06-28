d5141634a335f55584eac6e754199073cd8756cf
capt-alien <eric.botcher@presidio.edu>
2019-06-27 18:28:40 -0700

    Createing second app

--- hoodslam/wrestlers/models.py
+++ hoodslam/wrestlers/models.py
@@ -28,40 +28,8 @@ class Wrestler(models.Model):
 class Match(models.Model):
     date = models.DateField()
     # event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    #Having problems selecting only one wrestler
     wrestlers = models.ManyToManyField(Wrestler)
     summary = models.CharField(max_length=200, blank=True, null=True)
     outcome = models.CharField(max_length=200, blank=True, null=True)
-    #eventually we will be able to selelct multiple winners/loosers
     winners = models.ManyToManyField(Wrestler, blank=True, related_name="winners") #,choices=wrestlers)
     losers = models.ManyToManyField(Wrestler, blank=True, related_name="losers") #,choices=wrestlers)
-    #post_save -->
-
-def update_stats(sender, instance, created, **kwargs):
-    """This method will automatically go through the
-        winners and loosers and update the stats of the
-        wrestlers in the db"""
-    if created:
-        print(instance.winners.all())
-        for winner in instance.winners.all():
-            print(f'winner.name: {winner.name}')
-            #find object in db
-            new_win = Wrestler.objects.filter(name=winner.name)
-            #update wins
-            new_win.wins = new_win.wins + 1
-            new_win.save()
-        for loser in instance.losers.all():
-            #find object in db
-            new_loss = Wrestler.objects.filter(name=loser.name)
-            #update wins
-            new_loss.losses = new_loss.losses + 1
-            new_loss.save()
-        return {'message': "Wins/losses recorded"}
-
-
-#signal
-m2m_changed.connect(update_stats, sender=Match)
-
-
-#Django signals (when this happens call this just hoook into a function)
-# https://docs.djangoproject.com/en/2.2/ref/signals/   django signals post_save
--- hoodslam/wrestlers/signals.py
+++ hoodslam/wrestlers/signals.py
@@ -1,20 +1,24 @@
-# from django.db.models.signals import post_save
-#
-#
-# def update_stats(Match):
-#     """This method will automatically go through the
-#         winners and loosers and update the stats of the
-#         wrestlers in the db"""
-#     for winner in Match.winners:
-#         #find object in db
-#         new_win = Wrestler.objects.filter(name=winner)
-#         #update wins
-#         new_win.wins += 1
-#         new_win.save()
-#     for loser in Match.loosers:
-#         #find object in db
-#         new_loss = Wrestler.objects.filter(name=loser)
-#         #update wins
-#         new_loss.losses += 1
-#         new_loss.save()
-#     return {'message': "Wins/losses recorded"}
+def update_stats(sender, instance, created, **kwargs):
+    """This method will automatically go through the
+        winners and loosers and update the stats of the
+        wrestlers in the db"""
+    if created:
+        print(instance.winners.all())
+        for winner in instance.winners.all():
+            print(f'winner.name: {winner.name}')
+            #find object in db
+            new_win = Wrestler.objects.filter(name=winner.name)
+            #update wins
+            new_win.wins = new_win.wins + 1
+            new_win.save()
+        for loser in instance.losers.all():
+            #find object in db
+            new_loss = Wrestler.objects.filter(name=loser.name)
+            #update wins
+            new_loss.losses = new_loss.losses + 1
+            new_loss.save()
+        return {'message': "Wins/losses recorded"}
+
+
+#signal
+m2m_changed.connect(update_stats, sender=Match)
